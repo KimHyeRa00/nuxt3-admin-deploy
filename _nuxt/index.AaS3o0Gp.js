7f210ce646805f5cfa29a6659463948eba276527
hrkim <hrkim@mannaplanet.co.kr>
2024-02-07 15:33:12 +0900

    Updates

---
+++ _nuxt/index.AaS3o0Gp.js
@@ -0,0 +1,1 @@
+import{u as e}from"./vue.f36acd1f.msv4FYu2.js";import{f as o,o as t,c as n}from"./entry.vY8tKIdr.js";const s={class:"text-white"},_=o({__name:"index",setup(a){return e({title:"Login"}),(c,i)=>(t(),n("div",s," Comming Soon "))}});export{_ as default};
